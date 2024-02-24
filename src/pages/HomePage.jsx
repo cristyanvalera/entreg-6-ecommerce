e9311b6a0c5a08a20345a5d24ef051beec7925e1
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-24 14:54:41 -0400

    filter by product name

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsThunk } from "../store/slices/products.slice";
 import { ProductCard } from "../components/HomePage/ProductCard";
+import './styles/homePage.css';
 
 export const HomePage = () => {
+    const [productName, setProductName] = useState('');
     const products = useSelector(store => store.products);
     const dispatch = useDispatch();
+    const search = useRef();
 
     useEffect(() => {
         let url = 'https://e-commerce-api-v2.academlo.tech/api/v1/products';
         dispatch(getProductsThunk(url));
     }, []);
 
+    const handleSearch = () => {
+        setProductName(search.current.value.toLowerCase().trim());
+    };
+
     return (
         <div>
             <h1>Ecommerce</h1>
-            <section>
-                {products?.map(prod => (
-                    <ProductCard key={prod.id} prod={prod} />
-                ))}
+
+            <div>
+                <input type="text" ref={search} onChange={handleSearch} />
+            </div>
+
+            <section className="product-container">
+                {products
+                    ?.filter((p) => p.title.toLowerCase().includes(productName))
+                    .map(prod => (
+                        <ProductCard key={prod.id} prod={prod} />
+                    ))}
             </section>
         </div>
     );
