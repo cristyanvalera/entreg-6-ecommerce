5ddcf6aad14f3272ef87f4994a46525d088d5e87
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-24 14:56:43 -0400

    fill the product card and info of the product

--- src/components/HomePage/ProductCard.jsx
+++ src/components/HomePage/ProductCard.jsx
@@ -1,8 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import './styles/productCard.css';
+
 export const ProductCard = ({ prod }) => {
-    console.log(prod);
+    const navigate = useNavigate();
+
+    const handleView = () => navigate(`/product/${prod.id}`);
+    
     return (
-        <div>
-            prod
-        </div>
+        <article className="product-card">
+            <figure>
+                <img src={prod.images[0].url} alt="product image" />
+            </figure>
+            
+            <div>
+                <ul>
+                    <li>
+                        <span>{prod.brand}</span>
+                        <span>{prod.title}</span>
+                    </li>
+
+                    <li>
+                        <span>Price</span>
+                        <span>$ {prod.price}</span>
+                    </li>
+                </ul>
+            </div>
+            
+            <div className='product-btns'>
+                <button>Add to cart</button>
+                
+                <button onClick={handleView}>
+                    View more
+                </button>
+            </div>
+        </article>
     );
 };
