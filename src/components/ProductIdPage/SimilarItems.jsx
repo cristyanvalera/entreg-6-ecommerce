71e1b879adeb91c63d578bdb5e29156131e8ca4e
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-24 14:56:11 -0400

    show similar items by category of the products

---
+++ src/components/ProductIdPage/SimilarItems.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { ProductCard } from "../HomePage/ProductCard";
+
+export const SimilarItems = ({ categoryId, prodId }) => {
+    const [productsByCategory, getProductsByCategory] = useFetch();
+
+    useEffect(() => {
+        if (categoryId) {
+            let url = `https://e-commerce-api-v2.academlo.tech/api/v1/products?categoryId=${categoryId}`;
+
+            getProductsByCategory(url);
+        }
+    }, [categoryId]);
+
+    return (
+        <section>
+            <h2>Discover similar items</h2>
+
+            <div>
+                {productsByCategory
+                    ?.filter(prod => prod.id !== +prodId)
+                    ?.map(product => (
+                        <ProductCard key={product.id} prod={product} />
+                    ))}
+            </div>
+        </section>
+    );
+};
--- src/pages/ProductIdPage.jsx
+++ src/pages/ProductIdPage.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { InfoProduct } from "../components/ProductIdPage/InfoProduct";
+import { useFetch } from "../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import { SimilarItems } from "../components/ProductIdPage/SimilarItems";
+
 export const ProductIdPage = () => {
+    const [productId, getProductId] = useFetch();
+    const param = useParams();
+
+    useEffect(() => {
+        let url = `https://e-commerce-api-v2.academlo.tech/api/v1/products/${param.id}`;
+        
+        getProductId(url);
+    }, [param]);
+    
     return (
-        <div>ProductIdPage</div>
+        <div>
+            <h2>idProduct</h2>
+
+            <InfoProduct productId={productId} />
+
+            <SimilarItems
+                categoryId={productId?.categoryId}
+                prodId={param.id}
+            />
+        </div>
     )
 };
