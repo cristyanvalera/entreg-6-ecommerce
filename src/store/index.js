7a2c4bbdff9fffa2c475f6c0563ac1d1ede1affe
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-25 17:15:56 -0400

    [WIP]: create cart slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import products from "./slices/products.slice";
+import cart from "./slices/cart.slice";
 
 export const store = configureStore({
-    reducer: { products },
+    reducer: { products, cart },
 });
