38eadd8dc05b755c265ece5b8f23256b959f548c
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-25 17:17:32 -0400

    support for register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,58 @@
+import { useForm } from "react-hook-form";
+import { useAuth } from "../hooks/useAuth";
+
 export const RegisterPage = () => {
+    const createUser = useAuth();
+    const { handleSubmit, register, reset } = useForm();
+
+    const resetAll = () => {
+        reset({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            phone: "",
+        });
+    };
+
+    const submit = (data) => {
+        let url = 'https://e-commerce-api-v2.academlo.tech/api/v1/users';
+
+        createUser(url, data);
+        
+        resetAll();
+    };
+
     return (
-        <div>RegisterPage</div>
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <div>
+                    <label htmlFor="firstName">Name</label>
+                    <input {...register('firstName')} id="firstName" type="text" />
+                </div>
+
+                <div>
+                    <label htmlFor="lastName">Last name</label>
+                    <input {...register('lastName')} id="lastName" type="text" />
+                </div>
+
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input {...register('email')} id="email" type="email" />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input {...register('password')} id="password" type="password" />
+                </div>
+
+                <div>
+                    <label htmlFor="phone">Phone</label>
+                    <input {...register('phone')} id="phone" type="number" />
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 };
