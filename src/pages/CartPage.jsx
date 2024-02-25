b979c013bd630571c4979754d50af0b21949cbc3
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-25 17:24:53 -0400

    create cart component

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 export const CartPage = () => {
+    const cart = useSelector(store => store.cart);
+    console.log(cart);
     return (
         <div>CartPage</div>
     );
