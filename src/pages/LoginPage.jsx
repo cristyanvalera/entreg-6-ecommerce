6bc9f1dfad6f3ce5df83031a385f3b6adfb1cd6c
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-25 17:17:43 -0400

    support for login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,33 @@
+import { useForm } from "react-hook-form";
+import { useAuth } from "../hooks/useAuth";
+
 export const LoginPage = () => {
+    const { handleSubmit, register, reset } = useForm();
+    const createToken = useAuth();
+
+    const submit = (data) => {
+        let url = 'https://e-commerce-api-v2.academlo.tech/api/v1/users/login';
+
+        createToken(url, data);
+
+        reset({ email: '', password: '' });
+    };
+
     return (
-        <div>LoginPage</div>
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input {...register('email')} id="email" type="email" />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input {...register('password')} id="password" type="password" />
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 };
