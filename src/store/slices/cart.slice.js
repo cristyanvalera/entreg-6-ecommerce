a8cc06377b40a9a795b6ef5b438eec0b19381a0f
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-28 23:03:32 -0400

    crud action in cart slice

--- src/store/slices/cart.slice.js
+++ src/store/slices/cart.slice.js
@@ -1,12 +1,59 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { getToken } from "../../utils/getToken";
+const urlBase = 'https://e-commerce-api-v2.academlo.tech/api/v1/cart';
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: [],
     reducers: {
+        addToCart: (currentValue, action) => [
+            ...currentValue,
+            action.payload,
+        ],
+        
+        deleteFromCart: (currentValue, action) => {
+           return  currentValue.filter(product => (
+                product.id !== action.payload
+            ));
+        },
+        
         setCart: (currentValue, action) => action.payload,
+        
+        updateCart: (currentValue, action) => {
+            const { id, quantity } = action.payload;
+
+            currentValue.map(product => (
+                product.id === id
+                    ? product.quantity = quantity
+                    : product
+            ))
+        },
     },
 });
 
-export const { setCart } = cartSlice.actions;
+export const { addToCart, deleteFromCart, setCart, updateCart } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export const getCartThunk = () => (dispatch) => {
+    axios.get(urlBase, getToken())
+        .then(response => dispatch(setCart(response.data)))
+        .catch(error => console.log(error));
+};
+
+export const postCartThunk = (data) => (dispatch) => {
+    axios.post(urlBase, data, getToken())
+        .then(response => dispatch(addToCart(response.data)))
+        .catch(error => console.log(error));
+};
+
+export const deleteCartThunk = (id) => (dispatch) => {
+    let url = `${urlBase}/${id}`;
+
+    axios.delete(url, getToken())
+        .then(response => {
+            console.log('Item deleted', response.data);
+            dispatch(deleteFromCart(id));
+        })
+        .catch(error => console.log(error));
+};
