32fdb5f48402728e8b8642c1af16d03a69b0bb72
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-29 22:48:21 -0400

    handle quantity of products in the shopping cart

--- src/components/CartPage/CartProducts.jsx
+++ src/components/CartPage/CartProducts.jsx
@@ -1,12 +1,22 @@
 import { useDispatch } from 'react-redux';
 import './styles/cartProducts.css';
-import { deleteCartThunk } from '../../store/slices/cart.slice';
+import { deleteCartThunk, updateCartThunk } from '../../store/slices/cart.slice';
 
 export const CartProducts = ({ product }) => {
     const dispatch = useDispatch();
     
     const handleRemove = () => {
         dispatch(deleteCartThunk(product.id));
+    };
+
+    const handleLess = () => {
+        if (product.quantity > 1) {
+            dispatch(updateCartThunk(product, -1));
+        }
+    };
+
+    const handleMore = () => {
+        dispatch(updateCartThunk(product, 1));
     };
 
     return (
@@ -19,11 +29,13 @@ export const CartProducts = ({ product }) => {
                 <div>
                     <h2>{product.product.title}</h2>
 
-                    <button>-</button>
-                    <span>{product.quantity}</span>
-                    <button>+</button>
+                    <button onClick={handleLess}>-</button>
 
-                    <h3>$ {product.product.price}</h3>
+                    <span>{product.quantity}</span>
+                    
+                    <button onClick={handleMore}>+</button>
+
+                    <h3>$ Total: {product.product.price * product.quantity}</h3>
                 </div>
 
                 <button onClick={handleRemove}>Delete</button>
